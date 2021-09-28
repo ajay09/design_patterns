c7d03e22c0437bc43e9d339b0cef845c72062d1d
ajay <ajay199109@gmail.com>
2021-09-28 23:59:58 +0530

    Basic Decorator

---
+++ structural/4_decorator/1_basic_decorator_1.cpp
@@ -0,0 +1,69 @@
+/*
+	Decorator Pattern
+*/
+
+////////////////////////////////////
+////////////  Component.h
+////////////////////////////////////
+
+class Component {
+public:
+	virtual void Operation() = 0;
+	~Component() = default;
+};
+
+
+////////////////////////////////////
+////////////  ConcreteComponent.h
+////////////////////////////////////
+#include <iostream>
+
+class ConcreteComponent : public Component {
+public:
+	void Operation() override {
+		std::cout << "[ConcreteComponent] Operation invoked\n";
+	}
+};
+
+
+// Lets implement the concrete decorator directly
+// to understand the importance of defining a Decorator base class.
+////////////////////////////////////
+////////////  ConcreteDecoratorA.h
+////////////////////////////////////
+class ConcreteDecoratorA : public Component {
+	Component *m_ptr{};
+public:
+	ConcreteDecoratorA(Component *cmp) : m_ptr{cmp} {}
+	void Operation() override {
+		std::cout << "[ConcreteDecoratorA] Operation invoked\n";
+		m_ptr->Operation();
+	}
+};
+
+
+////////////////////////////////////
+////////////  ConcreteDecoratorB.h
+////////////////////////////////////
+class ConcreteDecoratorB : public Component {
+	Component *m_ptr{};
+public:
+	ConcreteDecoratorB(Component *cmp) : m_ptr{cmp} {}
+	void Operation() override {
+		std::cout << "[ConcreteDecoratorB] Operation invoked\n";
+		m_ptr->Operation();
+	}
+};
+
+
+
+int main() {
+	ConcreteComponent component{};
+	ConcreteDecoratorA decA{component};
+	decA.Operation();
+	return 0;
+}
+
+
+
+
